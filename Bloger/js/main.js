02a60b6b9d1088dc47cb11f6525504bbd17054e4
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-07-21 14:51:39 -0700

    Tours

--- Bloger/js/main.js
+++ Bloger/js/main.js
@@ -1,3 +1,15 @@
+
+const responsive={
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
 $(document).ready(function(){
 $nav=$('.nav');
 $toggleCollapse=$('.toggle-collapse');
@@ -6,13 +18,23 @@ $toggleCollapse=$('.toggle-collapse');
 $toggleCollapse.click(function(){
     $nav.toggleClass('collapse')
 });
-});
 
-//Owl carousel for blog
 $('.owl-carousel').owlCarousel({
     loop:true,
     autoplay:true,
     autoplayTimeout:3000,
     dots:false,
-    nav:true
+    nav:true,
+    responsive:responsive
 });
+$('.move-upward span ').click(function(){
+    $('html,body').animate({
+        scrollTop:0
+    },1000);
+});
+
+});
+
+//Owl carousel for blog
+
+//click to scroll top
